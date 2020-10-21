48f34ac3585a398a8bd975ba2d0c64952393d0a3
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2020-10-21 16:36:00 +0200

    Add HELOC dataset and readme

---
+++ Data_Cleansing/heloc.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+filePath = '../Datasets/HELOC/'
+fileName = 'heloc_dataset_v1.csv'
+
+# Read Data from csv
+heloc_df = pd.read_csv(filePath + fileName)
+
+# change column 'RiskPerformance' in Bad = 0 and Good = 1
+if 'RiskPerformance' in heloc_df:
+    heloc_df['RiskPerformance_binary'] = 0
+    heloc_df.loc[heloc_df['RiskPerformance'].isin(['Good']), 'RiskPerformance_binary'] = 1
+    heloc_df['RiskPerformance'] = heloc_df['RiskPerformance_binary']
+    heloc_df.drop('RiskPerformance_binary', axis=1, inplace=True)
+
+# Drop rows with value -9 in every column
+label_risk = heloc_df.pop('RiskPerformance')
+label_risk = label_risk[(heloc_df > -9).any(axis=1)]
+heloc_df = heloc_df.loc[(heloc_df > -9).any(axis=1)]
+heloc_df['RiskPerformance'] = label_risk
+
+# Drop all columns with special values. Warning: only 2,500 rows will remain
+# heloc_df = heloc_df[heloc_df >= 0]
+# heloc_df.dropna(axis=0, inplace=True)
+
+# set remaining special values with default values
+# Max Month for values with -7
+# Average values of its specific class for remaining special values
+x_cols = list(heloc_df.columns.values)
+
+for col in x_cols:
+    # compute max and average values of specific column
+    max_value = heloc_df[col].max()
+    avg_value_good = heloc_df.loc[(heloc_df['RiskPerformance'] == 1) & (heloc_df[col] >= 0), col].mean()
+    avg_value_bad = heloc_df.loc[(heloc_df['RiskPerformance'] == 0) & (heloc_df[col] >= 0), col].mean()
+    # set max values for all columns with value -7
+    heloc_df.loc[heloc_df[col] == -7, col] = max_value
+    heloc_df.loc[(heloc_df['RiskPerformance'] == 0) & (heloc_df[col].isin([-8, -9])), col] = avg_value_bad
+    heloc_df.loc[(heloc_df['RiskPerformance'] == 1) & (heloc_df[col].isin([-8, -9])), col] = avg_value_good
+
+# save cleansed data in edited folder
+heloc_df.to_csv(filePath + fileName, index=False)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Benchmarkin_Counterfactual_Examples
+# Benchmarkin_Counterfactual_Examples  
+
+## Datasets
+### German Credit Score  
+There are two versions of this data. The file german_numeric consists of already transformed data.  
+
+### Give Me Credit
+Test dataset is missing information for column 'SeriousDlquin2yrs' which is used in the training set as label.  
+All NaN-values are dropped. 
+
+### HELOC  
+This dataset consists of the following special values    
+
+- -9: No bureau record or no investigation
+- -8: No usable/valid trades or inquiries
+- -7: Condition not met (e.g. no inquiries, no delinquancies)
+
+The preprocessing from 'https://github.com/Trusted-AI/AIX360/blob/master/aix360/datasets/heloc_dataset.py'
+treats them as follows:
+- all rows with only -9 are dropped and all remaining special values are declared as NaN
+- or all special values are set to default value 0
+
+Since the default value of 0 is mostly irritating (zero values could mean for a column like 
+'Month since most recent delinquency' that there is a recent delinquency, where there is no delinquency
+for special value -7), we choosed the following default values:
+- Rows which contains only -9 values are dropped
+- All cells with -7 are receiving max values of its column, to avoid the problem mentioned above
+- Cells with -8, -9 are set to average values of its class (good, bad) and column
