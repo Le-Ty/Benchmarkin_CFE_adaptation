e1ffc29b4dec1bf7ee9c94293e88d27e49e76bd3
Sascha <sascha.bielawski@student.uni-tuebingen.de>
2020-10-30 14:54:43 +0100

    Add distance benchmarking

--- CF_Examples/DICE/cf_ann_adult.py
+++ CF_Examples/DICE/cf_ann_adult.py
@@ -1,52 +1,61 @@
-import torch
 import pickle
 import pandas as pd
 
-from torch import nn
-
-import ML_Model.ANN.model as model
 import CF_Models.dice_ml as dice_ml
-import library.data_processing as processing
 
-from CF_Models.dice_ml.utils import helpers
 
-# import dataset
-path = '../../Datasets/Adult/'
-file_name = 'adult_full.csv'
-dataset = pd.read_csv(path + file_name)
+def get_counterfactual(dataset_path, dataset_filename, target_name, model, features, number_of_cf):
+    """
+    Compute counterfactual for DICE
+    :param dataset_path: String; Path to folder of dataset
+    :param dataset_filename: String; Filename
+    :param target_name: String, Class of label
+    :param model: Pytorch model
+    :param features: List of continuous feature
+    :param number_of_cf: Number of counterfactuals to compute
+    :return: Counterfactual object
+    """
+    # import dataset
+    # path = '../../Datasets/Adult/'
+    path = dataset_path
+    # file_name = 'adult_full.csv'
+    file_name = dataset_filename
+    dataset = pd.read_csv(path + file_name)
 
-# get name of categorical features
-with open(path + 'cat_names.txt', 'rb') as f:
-    categorical_features = pickle.load(f)
+    # get name of categorical features
+    with open(path + 'cat_names.txt', 'rb') as f:
+        categorical_features = pickle.load(f)
 
-# normalize continuous features
-target_name = 'income'
-# dataset = processing.normalize(dataset, target_name)
+    # normalize continuous features
+    # target_name = 'income'
+    # dataset = processing.normalize(dataset, target_name)
 
-# load ML model
-model_path = '../../ML_Model/Saved_Models/ANN/2020-10-29_13-13-55_input_104_lr_0.002_te_0.34.pt'
-ann = model.ANN(104, 64, 16, 8, 1)
-ann.load_state_dict(torch.load(model_path))
+    # load ML model
+    # model_path = '../../ML_Model/Saved_Models/ANN/2020-10-29_13-13-55_input_104_lr_0.002_te_0.34.pt'
+    # ann = model.ANN(104, 64, 16, 8, 1)
+    # ann.load_state_dict(torch.load(model_path))
+    ann = model
 
-# build dice model
-# given our data preprocessing we do not need dice to one-hot-encode our categorial features
-features = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'hours-per-week', 'capital-loss']
-# features = list(dataset.columns)
-# features.remove(target_name)
-dice_data = dice_ml.Data(dataframe=dataset, continuous_features=features, outcome_name=target_name)
-dice_model = dice_ml.Model(model=ann, backend='PYT')
+    # build dice model
+    # given our data preprocessing we do not need dice to one-hot-encode our categorial features
+    # features = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'hours-per-week', 'capital-loss']
+    features = features
+    # features = list(dataset.columns)
+    # features.remove(target_name)
+    dice_data = dice_ml.Data(dataframe=dataset, continuous_features=features, outcome_name=target_name)
+    dice_model = dice_ml.Model(model=ann, backend='PYT')
 
-# initiate DICE
-exp = dice_ml.Dice(dice_data, dice_model)
+    # initiate DICE
+    exp = dice_ml.Dice(dice_data, dice_model)
 
-# query instance to create examples
-query_instance = dataset.iloc[22].to_dict()
-del query_instance[target_name]
+    # query instance to create examples
+    query_instance = dataset.iloc[22].to_dict()
+    del query_instance[target_name]
 
-# generate counterfactuals
-dice_exp = exp.generate_counterfactuals(query_instance, total_CFs=4, desired_class="opposite")
+    # generate counterfactuals
+    dice_exp = exp.generate_counterfactuals(query_instance, total_CFs=number_of_cf, desired_class="opposite")
 
-# visualize the result
-dice_exp.visualize_as_dataframe()
+    # visualize the result
+    # dice_exp.visualize_as_dataframe()
 
-# print(query_instance)
+    return dice_exp
---
+++ benchmarking.py
@@ -0,0 +1,40 @@
+import torch
+
+import ML_Model.ANN.model as model
+import CF_Examples.DICE.cf_ann_adult as dice_examples
+import library.measure as measure
+
+
+def main():
+    # Get DICE counterfactuals for Adult Dataset
+    data_path = 'Datasets/Adult/'
+    data_name = 'adult_full.csv'
+    target_name = 'income'
+    features = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'hours-per-week', 'capital-loss']
+
+    model_path = 'ML_Model/Saved_Models/ANN/2020-10-29_13-13-55_input_104_lr_0.002_te_0.34.pt'
+    ann = model.ANN(104, 64, 16, 8, 1)
+    ann.load_state_dict(torch.load(model_path))
+
+    dice_adult_cf = dice_examples.get_counterfactual(data_path, data_name, target_name, ann, features, 1)
+
+    test_instance = dice_adult_cf.org_instance.values.tolist()[0]
+    counterfactuals = dice_adult_cf.final_cfs_list_sparse
+    counterfactual = counterfactuals[0]
+    data = dice_adult_cf.data_interface.data_df
+
+    d1 = measure.distance_d1(test_instance, counterfactual)
+    d2 = measure.distance_d2(test_instance, counterfactual, data)
+    d3 = measure.distance_d3(test_instance, counterfactual, data)
+    d4 = measure.distance_d4(test_instance, counterfactual)
+
+    print('Measurement results for DICE and Adult')
+    print('dist(x; x^F)_1: {}'.format(d1))
+    print('dist(x; x^F)_2: {}'.format(d2))
+    print('dist(x; x^F)_3: {}'.format(d3))
+    print('dist(x; x^F)_4: {}'.format(d4))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
